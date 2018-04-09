42f108f2216cc7b832cab693b6a37ca5afc9a3b6
Fabrício <fabricio-gf@hotmail.com>
2018-04-09 13:30:29 -0300

    Added click and score manager
    Simple click to gain points implemented

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour {
+
+    public float ClickGain;
+
+    public ScoreManager SM;
+
+    private void Start()
+    {
+        print("Start");
+
+    }
+
+    public void Click()
+    {
+        print("Click");
+        SM.UpdateScore(ClickGain);
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public float Score;
+    public Text ScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        ScoreDisplay.text = "0";
+	}
+	
+    public void UpdateScore(float gain)
+    {
+        Score += gain;
+        ScoreDisplay.text = Score.ToString();
+        print("Score: " + Score);
+    }
+}
