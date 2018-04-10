746076ee4878a45d2703caef2aef869f1a7a4bd9
Rodrigo Geurgas <rodrigogergas@gmail.com>
2018-04-09 22:26:21 -0300

    IpS
    IpS implemented

---
+++ Assets/Scripts/IpSManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IpSManager : MonoBehaviour {
+
+	[SerializeField] [Range(0, 1)] private float era = 0.01f;
+	[SerializeField] private float IpSGain;
+	[SerializeField] private ScoreManager SM;
+
+	private float time;
+
+	private void Start()
+	{
+		time = 0;
+	}
+
+	void Update () 
+	{
+		time += Time.deltaTime;
+
+		if (time >= era)
+		{
+			SM.UpdateScore (IpSGain * era);
+		}
+	}
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
-
     public float Score;
     public Text ScoreDisplay;
 
-	// Use this for initialization
 	void Start () {
         ScoreDisplay.text = "0";
 	}
 	
     public void UpdateScore(float gain)
     {
-        Score += gain;
-        ScoreDisplay.text = Score.ToString();
-        print("Score: " + Score);
+		Score += gain;
+		ScoreDisplay.text = Math.Round(Score, 1).ToString();
     }
 }
