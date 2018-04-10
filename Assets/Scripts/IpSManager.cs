70788c0920fc6b8c3926e8d31627ea8d5d9f8563
Rodrigo Geurgas <rodrigogergas@gmail.com>
2018-04-10 18:39:13 -0300

    IpS display + increase values of IpS

--- Assets/Scripts/IpSManager.cs
+++ Assets/Scripts/IpSManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IpSManager : MonoBehaviour {
 
 	[SerializeField] [Range(0, 1)] private float era = 0.01f;
 	[SerializeField] private float IpSGain;
-	[SerializeField] private ScoreManager SM;
+	[SerializeField] private float multiplier;
+	[SerializeField] private Text IpSDisplay;
 
+	private ScoreManager SM;
 	private float time;
 
 	private void Start()
 	{
 		time = 0;
+		SM = gameObject.GetComponent<ScoreManager> ();
+		IpSDisplay.text = "+0 IpS";
 	}
 
 	void Update () 
@@ -21,7 +26,18 @@ public class IpSManager : MonoBehaviour {
 
 		if (time >= era)
 		{
-			SM.UpdateScore (IpSGain * era);
+			SM.UpdateScore (IpSGain * multiplier * era);
 		}
+	}
+
+	void IncreaseIpS (int value) 
+	{
+		IpSGain += value;
+		IpSDisplay.text = "+" + (IpSGain * multiplier).ToString () + " IpS";
+	}
+
+	void IncreaseMult (float mult)
+	{
+		multiplier += mult;
 	}
 }
