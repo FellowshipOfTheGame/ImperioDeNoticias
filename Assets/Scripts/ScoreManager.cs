688755ab321bfb894c2939e55afbb42ee4b41e01
Rodrigo Geurgas <rodrigogergas@gmail.com>
2018-04-11 19:56:35 -0300

    Fix cagada

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
+    public static ScoreManager Instance;
+
     public float Score;
     public Text ScoreDisplay;
 
-	void Start () {
+    private void Awake()
+    {
+        //Check if instance already exists
+        if (Instance == null)
+            //if not, set instance to this
+            Instance = this;
+
+        //If instance already exists and it's not this:
+        else if (Instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
         ScoreDisplay.text = "0";
 	}
 	
